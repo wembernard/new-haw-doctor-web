2a127cc8fe1dc42d963425408349384bc547ea2d
Maxime Bernard <mbernard@wemanity.com>
2016-08-05 16:16:50 +0200

    Autoredirect to login when API return 401

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,5 +1,7 @@
 import { Http, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 declare var process: any;
 
 @Injectable()
@@ -7,7 +9,7 @@ export class ApiService {
   url: string;
   requestOptions: RequestOptions;
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private router: Router) {
     // this.url = 'http://api.demo.haw.k4met.com/api/';
     // this.url = 'http://api.dev.temp.k4met.com/api/';
     this.url = 'http://api.demo.temp.k4met.com/api/';
@@ -19,6 +21,8 @@ export class ApiService {
   };
 
   call(resource: string, verb: string = 'get', body?: Object) {
+    let promise;
+
     this.prepareHeaders();
 
     if (process.env.NODE_ENV !== 'production') {
@@ -26,10 +30,20 @@ export class ApiService {
     }
 
     if (['post', 'put', 'patch'].indexOf(verb) >= 0) {
-      return this.http[verb](this.url + resource, body, this.requestOptions).toPromise();
+      promise = this.http[verb](this.url + resource, body, this.requestOptions).toPromise();
     } else {
-      return this.http[verb](this.url + resource, this.requestOptions).toPromise();
+      promise = this.http[verb](this.url + resource, this.requestOptions).toPromise();
     }
+    
+    return promise.catch(err => {
+      /** Unauthorized */
+      if (err.status === 401) {
+        // store the current URL for redirecting back after login
+        // this.authService.redirectUrl = this.router.url; // Cause dependency injection trouble 
+        // navigate to the login page
+        this.router.navigate(['/login']);
+      }
+    })
   };
 
   private prepareHeaders() {
