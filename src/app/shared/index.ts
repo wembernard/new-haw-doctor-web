04a29572d63ae07f4d3b42f757136c2200f6a80d
Maxime Bernard <mbernard@wemanity.com>
2016-08-08 18:23:58 +0200

    MedicalForm Component on the way

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -4,3 +4,4 @@ export * from './api.service';
 export * from './auth.service';
 export * from './auth.guard';
 export * from './header/header.component';
+export * from './medical-form/medical-form.component';
---
+++ src/app/shared/medical-form/medical-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ApiService } from '../../shared';
+
+@Component({
+  selector: 'medical-form',
+  template: require('./medical-form.component.html'),
+  styles: [String(require('./medical-form.component.scss'))],
+  providers: [ApiService]
+})
+
+export class MedicalFormComponent implements OnInit {
+  results: any;
+
+  constructor(private api: ApiService) { }
+
+  @Input('medicalExamId') medicalExamId: number;
+
+  ngOnInit() {
+    /* Answers from medicalExam */
+    this.api.call('medicalExams/' + this.medicalExamId + '/answers?filter[include]=question').then(res => {
+      let answers = res.json() || [];
+      this.results = { outcome: 0, themes: [] };
+      for (let answer of answers) {
+        /** BUILDING DATA STRUCTURE */
+        // Build themes array
+        if (!this.results.themes[answer['question']['theme']]) {
+          this.results.themes[answer['question']['theme']] = { outcome: 0, subthemes: [] };
+        }
+        /** Build subthemes array */
+        if (!this.results.themes[answer['question']['theme']].subthemes[answer['question']['subtheme']]) {
+          this.results.themes[answer['question']['theme']].subthemes[answer['question']['subtheme']] = { outcome: 0, answers: [] };
+        }
+
+        /** FEED ANSWER */
+        this.results.themes[answer['question']['theme']].subthemes[answer['question']['subtheme']].answers.push(answer);
+        
+        /** UPDATING OUTCOME */
+        // Update subtheme outcome if necessary
+        if (this.results.themes[answer['question']['theme']].subthemes[answer['question']['subtheme']].outcome < answer.outcome) {
+          this.results.themes[answer['question']['theme']].subthemes[answer['question']['subtheme']].outcome = answer.outcome;
+        }
+        // Update theme outcome if necessary
+        if (this.results.themes[answer['question']['theme']].outcome < answer.outcome) {
+          this.results.themes[answer['question']['theme']].outcome = answer.outcome;
+        }
+        // Update results outcome if necessary
+        if (this.results.outcome < answer.outcome) {
+          this.results.outcome = answer.outcome;
+        }
+      }
+      console.log(this.results);
+    });
+  }
+}
