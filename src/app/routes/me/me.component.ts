96061941e3406854b88e8a6dae89117aef57b72c
Lionel Cassier <lionel.cassier@kametventures.com>
2016-08-17 18:38:37 +0200

    profile rerooted and date working

--- src/app/routes/me/me.component.ts
+++ src/app/routes/me/me.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 import { ApiService, AuthService } from '../../shared';
 
 @Component({
@@ -11,22 +11,34 @@ import { ApiService, AuthService } from '../../shared';
 
 export class MeComponent implements OnInit {
   profile;
-  // dateRegex;
+  
+  constructor(private api: ApiService, public authService: AuthService,private router: Router) { }
 
-  constructor(private api: ApiService, public authService: AuthService) { }
+   formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
 
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+  }
   ngOnInit() {
-    // this.dateRegex='/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])';
     let userId = this.authService.getUserId();
     this.api.call('doctors/' + userId).then((res) => {
       this.profile = res.json() || {};
+      this.profile.birthdate = this.formatDate(this.profile.birthdate);
     });
   }
 
   onSubmit() {
      // for (let i in )
     this.api.call('doctors/', 'post', this.profile).then((res) => { // RETURN 401 UNAUTHORIZED
-      console.log(res);
-    });
+      if(res) {
+        this.router.navigate(['/dashboard']);
+      } // Todo : catch the error by saying it didn't work
+    })
   }
 }
